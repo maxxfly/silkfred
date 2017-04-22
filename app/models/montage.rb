49db5303d754b6edf6c3e8482b23c89b372c44a2
JeanMary <jeanmary@gmail.com>
2017-04-22 21:52:21 +0200

    add a nice logo panda, refactorize

--- app/models/montage.rb
+++ app/models/montage.rb
@@ -37,24 +37,22 @@ class Montage <  ActiveRecord::Base
     # launch service
     montage_service = MontageService.new(image_1: images[0], image_2: images[1], logo: logo[0], margin: 20)
 
-    #path_output_file = Rails.root.join('public', 'photos', "montage_" + id.to_s + '.jpg')
-    #assemble = montage_service.assemble(path_output_file)
-
     assemble = montage_service.assemble
     assemble.format = "jpg"
-
-    self.status = "done"
 
     # push the controller that display the img by the base
     self.path = HOST + "/montages/" + id.to_s + ".jpg"
 
-    # push the local path
-    #self.path = HOST + "/photos/montage_" + id.to_s + '.jpg'
-
     # trick to avoid S3 for heroku, we save the base64 in database
     self.base64 = Base64.encode64(assemble.to_blob)
 
+    # push the local path
+    # path_output_file = Rails.root.join('public', 'photos', "montage_" + id.to_s + '.jpg')
+    # assemble = montage_service.assemble(path_output_file)
+    # self.path = HOST + "/photos/montage_" + id.to_s + '.jpg'    
     # self.base64 = Base64.encode64(open(path_output_file).read)
+
+    self.status = "done"
     self.save
 
     self.batch_montage.check_status
